1653f0d9821579edc010fab11c2a1990d8f39d44
Brendan Gould <brendan.gould314@gmail.com>
2021-05-14 13:15:31 -0600

    OpenGL Error checking

--- nutella/src/Nutella/Renderer/Shader.cpp
+++ nutella/src/Nutella/Renderer/Shader.cpp
@@ -2,28 +2,29 @@
 #include "Nutella/Renderer/Shader.hpp"
 
 #include "glad/glad.h"
+#include "Platform/OpenGL/glutil.hpp"
 
 namespace Nutella {
 	Shader::Shader(std::string& vertexSrc, std::string& fragmentSrc) {
 		// create shader object
-		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		GL_CALL(GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER));
 
 		// write source code into shader + compile
 		const GLchar* glVertexSrc = (const GLchar*) vertexSrc.c_str();
-		glShaderSource(vertexShader, 1, &glVertexSrc, NULL);
-		glCompileShader(vertexShader);
+		GL_CALL(glShaderSource(vertexShader, 1, &glVertexSrc, NULL));
+		GL_CALL(glCompileShader(vertexShader));
 
 		// check for compilation failure
 		GLint isCompiled = 0;
-		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
+		GL_CALL(glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled));
 		if (isCompiled != GL_TRUE) {
 			GLint errorLen = 0;
-			glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &errorLen);
+			GL_CALL(glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &errorLen));
 
 			std::vector<GLchar> errorMsg(errorLen);
-			glGetShaderInfoLog(vertexShader, errorLen, &errorLen, &errorMsg[0]);
+			GL_CALL(glGetShaderInfoLog(vertexShader, errorLen, &errorLen, &errorMsg[0]));
 
-			glDeleteShader(vertexShader);
+			GL_CALL(glDeleteShader(vertexShader));
 
 			NT_CORE_ERROR("Vertex shader compilation failed!");
 			NT_CORE_ERROR("{0}", errorMsg.data());
@@ -32,25 +33,25 @@ namespace Nutella {
 		}
 
 		// create shader object
-		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		GL_CALL(GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER));
 
 		// write source code into shader + compile
 		const GLchar* glFragmentSrc = (const GLchar*) fragmentSrc.c_str();
-		glShaderSource(fragmentShader, 1, &glFragmentSrc, NULL);
-		glCompileShader(fragmentShader);
+		GL_CALL(glShaderSource(fragmentShader, 1, &glFragmentSrc, NULL));
+		GL_CALL(glCompileShader(fragmentShader));
 
 		// check for compilation failure
 		isCompiled = 0;
-		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
+		GL_CALL(glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled));
 		if (isCompiled != GL_TRUE) {
 			GLint errorLen = 0;
-			glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &errorLen);
+			GL_CALL(glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &errorLen));
 
 			std::vector<GLchar> errorMsg(errorLen);
-			glGetShaderInfoLog(fragmentShader, errorLen, &errorLen, &errorMsg[0]);
+			GL_CALL(glGetShaderInfoLog(fragmentShader, errorLen, &errorLen, &errorMsg[0]));
 
-			glDeleteShader(vertexShader);
-			glDeleteShader(fragmentShader);
+			GL_CALL(glDeleteShader(vertexShader));
+			GL_CALL(glDeleteShader(fragmentShader));
 
 			NT_CORE_ERROR("Fragment shader compilation failed!");
 			NT_CORE_ERROR("{0}", errorMsg.data());
@@ -60,23 +61,23 @@ namespace Nutella {
 
 		// link shaders
 		m_RendererID = glCreateProgram();
-		glAttachShader(m_RendererID, vertexShader);
-		glAttachShader(m_RendererID, fragmentShader);
-		glLinkProgram(m_RendererID);
+		GL_CALL(glAttachShader(m_RendererID, 42));
+		GL_CALL(glAttachShader(m_RendererID, fragmentShader));
+		GL_CALL(glLinkProgram(m_RendererID));
 
 		// check for link failure
 		GLint isLinked = 0;
-		glGetProgramiv(m_RendererID, GL_LINK_STATUS, &isLinked);
+		GL_CALL(glGetProgramiv(m_RendererID, GL_LINK_STATUS, &isLinked));
 		if (isLinked != GL_TRUE) {
 			GLint errorLen = 0;
-			glGetProgramiv(m_RendererID, GL_INFO_LOG_LENGTH, &errorLen);
+			GL_CALL(glGetProgramiv(m_RendererID, GL_INFO_LOG_LENGTH, &errorLen));
 
 			std::vector<GLchar> errorMsg(errorLen);
-			glGetProgramInfoLog(m_RendererID, errorLen, &errorLen, &errorMsg[0]);
+			GL_CALL(glGetProgramInfoLog(m_RendererID, errorLen, &errorLen, &errorMsg[0]));
 
-			glDeleteShader(vertexShader);
-			glDeleteShader(fragmentShader);
-			glDeleteProgram(m_RendererID);
+			GL_CALL(glDeleteShader(vertexShader));
+			GL_CALL(glDeleteShader(fragmentShader));
+			GL_CALL(glDeleteProgram(m_RendererID));
 
 			NT_CORE_ERROR("Shader linking failed!");
 			NT_CORE_ERROR("{0}", errorMsg.data());
@@ -85,19 +86,19 @@ namespace Nutella {
 		}
 
 		// shader creation successful -> detach shaders
-		glDetachShader(m_RendererID, vertexShader);
-		glDetachShader(m_RendererID, fragmentShader);
+		GL_CALL(glDetachShader(m_RendererID, vertexShader));
+		GL_CALL(glDetachShader(m_RendererID, fragmentShader));
 	}
 
 	Shader::~Shader() {
-		glDeleteProgram(m_RendererID);
+		GL_CALL(glDeleteProgram(m_RendererID));
 	}
 
 	void Shader::Bind() {
-		glUseProgram(m_RendererID);
+		GL_CALL(glUseProgram(m_RendererID));
 	}
 
 	void Shader::Unbind() {
-		glUseProgram(0);
+		GL_CALL(glUseProgram(0));
 	}
 } // namespace Nutella
---
+++ nutella/src/Platform/OpenGL/glutil.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ntpch.hpp"
+#include "glad/glad.h"
+
+#include <signal.h>
+
+#ifndef NT_DIST
+static void glClearError() {
+	while (glGetError() != GL_NO_ERROR)
+		;
+}
+
+static bool glLogCall(const char* function, const char* file, int line) {
+	while (GLenum error = glGetError()) {
+		NT_CORE_ERROR("OpenGL Error: {0} in {1} in {2}:{3}", error, function, file, line);
+		return false;
+	}
+
+	return true;
+}
+
+	#define GL_CALL(x)                                                                             \
+		glClearError();                                                                            \
+		x;                                                                                         \
+		NT_CORE_ASSERT(glLogCall(#x, __FILE__, __LINE__), "OpenGL Error!")
+#else
+static void glClearError() {}
+
+static bool glLogCall(const char* function, const char* file, int line) {}
+
+	#define GL_CALL(x) x;
+
+#endif
